d0df2f25c9f07b90d59f33b51ea6ced3b869eb5d
Ezequiel <aleman_eze@hotmail.com>
2016-03-20 19:29:40 -0300

    Actualizado Calculadora.java

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -17,37 +17,9 @@ public Calculadora()
 	
 }
    public static void suma()
-   {
+   {	
 	   
 	   arreglo = new ArrayList<String>();
-	   sc = new Scanner(System.in);
-	   System.out.println(" INGRESE LA OPERACION: ");
-	   strToken = sc.next();
-	   //System.out.println(strToken);
-	   for(int i=0;i<strToken.length();i++)
-	   {     
-		   arreglo.add(Character.toString(strToken.charAt(i)));		   
-	   }
-	   for(int i=0;i<strToken.length();i++)
-	   {     
-		   if(arreglo.get(i)!="+")
-		   {
-			   A=Integer.parseInt(arreglo.get(i));
-		   }
-		   if(arreglo.get(i)!="-")
-		   {
-			   A=Integer.parseInt(arreglo.get(i));
-		   }
-		   else
-		   {
-			   op=arreglo.get(i).charAt(i);
-		   }
-		  
-	   }
-	   System.out.println("El tamaño del arreglo es: "+arreglo.size());
-	
-	   
-	   /*arreglo = new ArrayList<String>();
 	   sc = new Scanner(System.in);
 	   System.out.println(" INGRESE LA OPERACION: ");
 	   strToken = sc.next();
@@ -56,7 +28,7 @@ public Calculadora()
 	   {		   
 		   arreglo.add(Character.toString(strToken.charAt(i)));		   
 	   }
-	   System.out.println("El tamaño del arreglo es: "+arreglo.size());*/
+	   System.out.println("El tamaño del arreglo es: "+arreglo.size());
 	        
    }
   
