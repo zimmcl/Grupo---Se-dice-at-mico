0847ba8d9b1c952fc1fef983f7aa22e0b440bef9
Ezequiel <aleman_eze@hotmail.com>
2016-03-20 20:57:18 -0300

    Obtiene primer operador

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -2,34 +2,53 @@
 import javax.swing.JOptionPane;
 import java.util.ArrayList;
 import java.util.Scanner;
+//import java.math;
 //import java.lang.Object;
 
 public class Calculadora {
 	private static  ArrayList<String> arreglo;
 	private static Scanner sc;
-	private static String strToken;
-	private static int A;   
-	private static char op;   
-	//private static int Resultado;
+	private static String strToken; 
+	
 
 public Calculadora()
 {
 	
 }
    public static void suma()
-   {	
-	   
+   {
+	   //double Resultado=0;
+	   double operador1=0;
+	   double[] aux = new double[100];
+	   double[] aux2 = new double [100];
 	   arreglo = new ArrayList<String>();
 	   sc = new Scanner(System.in);
 	   System.out.println(" INGRESE LA OPERACION: ");
 	   strToken = sc.next();
 	   //System.out.println(strToken);
 	   for(int i=0;i<strToken.length();i++)
-	   {		   
-		   arreglo.add(Character.toString(strToken.charAt(i)));		   
+	   {	
+		   if(strToken.charAt(i)!='+'&& strToken.charAt(i)!='-')
+		   {
+			   aux[i]=Integer.parseInt(Character.toString(strToken.charAt(i)));
+		   } else {
+		   for(int j=0;j<i;j++)
+		   {
+			   aux2[j]=aux[i-j-1];
+			   aux2[j]=(aux2[j]*Math.pow(10,j+1))/10;
+			   operador1=operador1+aux2[j];
+		   }
+		  /* if(strToken.charAt(i)=='+')
+		   {
+			   Resultado = operador1+Integer.parseInt(arreglo.get(i));
+		   }*/
+		  // System.out.println(operador1+Integer.parseInt(arreglo.get(i)));
+		   }
+		   //arreglo.add(Character.toString(strToken.charAt(i)));		   
 	   }
-	   System.out.println("El tamaño del arreglo es: "+arreglo.size());
-	        
+	   //System.out.println("El tamaño del arreglo es: "+arreglo.size());
+	   
+	   System.out.println(operador1); 
    }
   
    
