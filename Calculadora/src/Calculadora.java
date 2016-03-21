589335d3201ff8907b08f65cc1252a900614bf33
Ezequiel <aleman_eze@hotmail.com>
2016-03-21 16:47:09 -0300

    Actualizacion codigo

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -17,38 +17,66 @@ public Calculadora()
 }
    public static void suma()
    {
-	   //double Resultado=0;
+	   double Resultado=0;
+	   char auxi='0';
 	   double operador1=0;
+	   //double operador2=0;
 	   double[] aux = new double[100];
 	   double[] aux2 = new double [100];
 	   arreglo = new ArrayList<String>();
 	   sc = new Scanner(System.in);
 	   System.out.println(" INGRESE LA OPERACION: ");
 	   strToken = sc.next();
-	   //System.out.println(strToken);
-	   for(int i=0;i<strToken.length();i++)
+	   for(int i=0;i<=strToken.length()-1;i++)
 	   {	
-		   if(strToken.charAt(i)!='+'&& strToken.charAt(i)!='-')
+		     
+		   if((strToken.charAt(i)!='+'&& strToken.charAt(i)!='-'))
 		   {
 			   aux[i]=Integer.parseInt(Character.toString(strToken.charAt(i)));
-		   } else {
+			   operador1=aux[i];
+		   } 
+		   else {
 		   for(int j=0;j<i;j++)
 		   {
 			   aux2[j]=aux[i-j-1];
 			   aux2[j]=(aux2[j]*Math.pow(10,j+1))/10;
 			   operador1=operador1+aux2[j];
+			   auxi=strToken.charAt(i);
 		   }
-		  /* if(strToken.charAt(i)=='+')
+		   if(i<strToken.length()-1)
 		   {
-			   Resultado = operador1+Integer.parseInt(arreglo.get(i));
-		   }*/
-		  // System.out.println(operador1+Integer.parseInt(arreglo.get(i)));
+			   arreglo.add(Double.toString(operador1));
+			   arreglo.add(Character.toString(auxi));
+			   operador1=0;	     
 		   }
+		   else
+		   {arreglo.add(Double.toString(aux[i-1]));}
+		   //VER!
+		   		   }
+		  
+			   
 		   //arreglo.add(Character.toString(strToken.charAt(i)));		   
+	   }
+	   //for(int i=0;i<2;i++)
+	   
+		   //System.out.println(arreglo.get(0));
+	   
+	   
+	   for(int k=0; k<=arreglo.size();k++)
+	   {
+		   if(arreglo.get(k)=="+")
+		   {
+			   Resultado=Resultado+Double.parseDouble(arreglo.get(k-1))+Double.parseDouble(arreglo.get(k+1));
+		   }
+		   if(arreglo.get(k)=="-")
+		   {
+			   Resultado=Resultado-(Double.parseDouble(arreglo.get(k-1))-Double.parseDouble(arreglo.get(k+1)));
+		   }
 	   }
 	   //System.out.println("El tamaño del arreglo es: "+arreglo.size());
 	   
-	   System.out.println(operador1); 
+	   //System.out.println(operador1);
+	   System.out.println(Resultado);
    }
   
    
