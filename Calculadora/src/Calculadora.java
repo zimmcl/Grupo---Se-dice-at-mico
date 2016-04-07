90b06757e0882af0572181f77bc3dabdde6559cb
Ezequiel <aleman_eze@hotmail.com>
2016-04-07 16:12:55 -0300

    Modifico Archivo

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -17,13 +17,13 @@ public static void Menu()
 	int opcion=0;
 	do
 	{
-	opcion = JOptionPane.showOptionDialog(null,"SELECCIONE OPERACION","MENU",JOptionPane.OK_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,new Object[] {"Suma y Resta","Multiplicación","Porcentaje","Salir"},"Salir");
+	opcion = JOptionPane.showOptionDialog(null,"SELECCIONE OPERACION","MENU",JOptionPane.OK_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,new Object[] {"Suma y Resta","Multiplicación","División","Salir"},"Salir");
 	switch(opcion){
 				case 0: SumaResta.sumarrestar();
 				break;
 				case 1: Multiplicacion.multiplicar();
 				break;
-				case 2: Porcentaje.porcentaje();
+				case 2: Division.dividir();
 				break;
 				case 3: JOptionPane.showMessageDialog(null,"Gracias por probar Calculadora");
 				break;
