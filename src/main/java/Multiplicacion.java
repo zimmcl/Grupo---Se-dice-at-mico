5dc3ea7320af8928e63591116313ce62503c5d0f
Gaspyarmando <gasparmando@hotmail.com>
2016-04-09 19:05:59 -0300

    Se Agrega operacion de Multiplicacion

--- src/main/java/Multiplicacion.java
+++ src/main/java/Multiplicacion.java
@@ -3,15 +3,15 @@ package main.java;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
-public class SumaResta {
+public class Multiplicacion {
 	
 	private static  ArrayList<String> arreglo;
 	private static  ArrayList<Double> arreglo2;
 	private static  ArrayList<Double> arreglo3;
 	private static  ArrayList<String> arreglo4;
 	private static String strToken;
-	
-	public static void sumarrestar()
+
+	public static void multiplicar()
 	{
 		char aux=' ';
 		double Resultado=0;
@@ -23,28 +23,30 @@ public class SumaResta {
 		
 		//-INGRESA OPERACION POR TECLADO-
 		strToken=JOptionPane.showInputDialog(null,"INGRESE LA OPERACION: ","Ingrese la Operacion");
-		//-VERIFICO OPERADOR "-" EN EL INICIO DE LA OPERACION-
-		if(strToken.charAt(0)=='-')
-		{
-			arreglo.add("0");
-		}
-		//-ALMACENO ELEMENTOS EN ARREGLO EXTRAIDOS DE LA CADENA "STRTOKEN"-
+		/*-VERIFICO OPERADOR "-" EN EL INICIO DE LA OPERACION.
+		   ALMACENO ELEMENTOS EN ARREGLO EXTRAIDOS DE LA CADENA "STRTOKEN"-*/
 		for(int i=0;i<strToken.length();i++)
 		{
-			arreglo.add(Character.toString(strToken.charAt(i)));
+			if(strToken.charAt(i)=='-')
+			{
+				arreglo.add("-1");
+				arreglo.add("*");
+			}else{
+			arreglo.add(Character.toString(strToken.charAt(i)));}
 		}
-		if(arreglo.contains("*")||arreglo.contains("/")||arreglo.contains("%"))
+		/*-VERIFICO QUE EL OPERADOR SEA EL CORRECTO-*/
+		if(arreglo.contains("+")||arreglo.contains("/")||arreglo.contains("%")||arreglo.contains("-"))
 		{
 			JOptionPane.showMessageDialog(null,"Error en el operador");
 			Calculadora.Menu();
 			System.exit(0);
 		}
 		arreglo.add("Fin");
-		/*-VERIFICA EXISTENCIA DE OPERADOR "+" O "-". LUEGO AGRUPO OPERANDOS. Ejemplo: strToken->[1,2,+,3,1] -> arreglo->[12,+,31]
+		/*-VERIFICA EXISTENCIA DE OPERADOR "*". LUEGO AGRUPO OPERANDOS. Ejemplo: strToken->[-,1,2,+,3,1] -> arreglo->[-1,*,12,*,31]
 		   SE UTILZAN ARREGLOS AUXILIARES PARA TAL FIN-*/
 		for(int i=0;i<arreglo.size();i++)
 		{
-			if(arreglo.get(i).equals("+") || arreglo.get(i).equals("-") || arreglo.get(i).equals("Fin"))
+			if(arreglo.get(i).equals("*") || arreglo.get(i).equals("Fin"))
 			   {
 				for(int j=0;j<arreglo2.size();j++)
 				{
@@ -67,20 +69,22 @@ public class SumaResta {
 			   }
 			   arreglo3.clear();
 		}
-		//-ELIMINO ULTIMO ELEMENTO DEFINIDO COMO "F"-	
+		//-ELIMINO ULTIMO ELEMENTO DEFINIDO COMO "F"-
 		arreglo4.remove(arreglo4.size()-1);
+		/*-VERIFICO QUE LA CANTIDAD DE OPERADORES NO SEA SUPERIOR A 2-*/
+		if(arreglo4.size()>3)
+		{
+			JOptionPane.showMessageDialog(null,"Cantidad de operandos superior a 2");
+			Calculadora.Menu();
+			System.exit(0);
+		}
 		arreglo3.add(Double.parseDouble(arreglo4.get(0)));
 		//-RESUELVO OPERACION-
 		for(int k=0; k<arreglo4.size()-1;k++)
 		   {
-			   if(arreglo4.get(k+1).equals("+"))
+			   if(arreglo4.get(k+1).equals("*"))
 			   {
-				   Resultado=(arreglo3.get(0))+Double.parseDouble(arreglo4.get(k+2));
-				   arreglo3.add(0,Resultado);
-			   }
-			   if(arreglo4.get(k+1).equals("-"))
-			   {
-				   Resultado=(arreglo3.get(0)-Double.parseDouble(arreglo4.get(k+2)));
+				   Resultado=(arreglo3.get(0))*Double.parseDouble(arreglo4.get(k+2));
 				   arreglo3.add(0,Resultado);
 			   }
 		   }
